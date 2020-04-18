da6e1660096fa8ed9bf84eb582483515316352eb
Mike Taylor <mike.taylor@alliedtelesis.co.nz>
2020-04-18 17:24:53 +1200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener("submit", e => {
   const location = search.value;
   messageTwo.textContent = "";
   messageOne.textContent = "Loading...";
-  fetch(`http://miketa-dl:3000/weather/?address=${location}`).then(res => {
+  fetch(`/weather/?address=${location}`).then(res => {
     res.json().then(data => {
       if (data.error) {
         messageOne.textContent = data.error;
